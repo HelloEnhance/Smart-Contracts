77e731cfc0f7ba2c2ae98c10c03f954da39d37ce
HelloEnhance <98238570+HelloEnhance@users.noreply.github.com>
2022-01-22 13:57:44 -0800

    Initial Commit

---
+++ GetStuck.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract GetStuck {
+    
+    function _getStuckTokens(IERC20 _token, address _receiver) internal {
+        _token.transfer(_receiver, _token.balanceOf(address(this)));
+    }
+
+    function _getStuckETH(address _receiver) internal {
+        payable(_receiver).call{value: address(this).balance}("");
+    }
+}
